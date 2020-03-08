0d47a863f68812c4de5eb3c2307f7e1258359930
Wagner Xu <wagnerhsu@hotmail.com>
2020-03-08 19:40:23 +0800

    Add htmlcssprimer

---
+++ MyCode/03/htmlcsspriner/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div> 
+    <h4 class="bg-primary text-white text-center p-2">
+      Adam's To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 fount-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      tasks: [{action:"Buy Flowers",done:false},
+      {action:"Get Shoes",done:false},
+      {action:"Collect Tickets",done:true},
+      {action:"Call Joe",done:false}]
+    }
+  }
+}
+</script>
+
+
