45629412a8b52ab48dbf36dad74d793229e072fd
Wagner Xu <wagnerhsu@hotmail.com>
2020-03-09 09:27:33 +0800

    Add jsprimer

--- MyCode/03/htmlcsspriner/src/App.vue
+++ MyCode/03/htmlcsspriner/src/App.vue
@@ -1,18 +1,30 @@
-<template>
-  <div> 
-    <h4 class="bg-primary text-white text-center p-2">
-      Adam's To Do List
-    </h4>
-    <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 fount-weight-bold">Done</div>
+﻿<template>
+  <div>
+    <h4 class="bg-primary text-white text-center p-2">Adam's To Do List</h4>
+    <table class="table table-striped table-bordered table-sm">
+      <thead>
+        <tr>
+          <th>Task</th>
+          <th>Done</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="t in tasks" v-bind:key="t.action">
+          <td>{{t.action}}</td>
+          <td>{{t.done}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+      <div class="form-group m-2">
+          <label>New Item:</label>
+          <input v-model="newItemText" class="form-control" />
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2">{{t.done}}</div>
+      <div class="text-center">
+          <button class="btn bg-primary" v-on:click="addNewTodo">
+              Add
+          </button>
       </div>
-    </div>
   </div>
 </template>
 
@@ -20,13 +32,25 @@
 export default {
   data: function() {
     return {
-      tasks: [{action:"Buy Flowers",done:false},
-      {action:"Get Shoes",done:false},
-      {action:"Collect Tickets",done:true},
-      {action:"Call Joe",done:false}]
-    }
-  }
-}
+      tasks: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get Shoes", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Call Joe", done: false }
+        ],
+        newItemText:""
+    };
+        },
+        methods: {
+            addNewTodo() {
+                this.tasks.push({
+                    action: this.newItemText,
+                    done: false
+                });
+                this.newItemText = "";
+            }
+        }
+};
 </script>
 
 
